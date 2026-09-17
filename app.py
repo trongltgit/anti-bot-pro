fd6c947268cfe285912bc7744257f61b685a47c4
trongltgit <tronglt.hcm@gmail.com>
2026-09-17 22:20:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ Chạy local: python app.py
 Production (Render): gunicorn app:app
 """
 
-from app import create_app
+from flask import Flask
 import os
 
-app = create_app()
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Anti-Bot Pro is running!"
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    app.run(host="0.0.0.0", port=port, debug=debug)
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=debug
+    )
